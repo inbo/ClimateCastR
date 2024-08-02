3d4828638386aa45ed6ff06a4323bc73d5b0de86
soriadelva <soria.delva@gmail.com>
2024-08-02 17:23:04 +0200

    Add last parts of get_zip_data() function

--- R/get_data.R
+++ R/get_data.R
@@ -351,6 +351,7 @@ get_gbif_data <- function(taxon_key,
                                       " were found on GBIF.")
   )
 
+
   #-----------------------------------------
   #         3. Clean data
   #-----------------------------------------
@@ -435,70 +436,81 @@ get_zip_data <- function(zip_file,
     )
   }
 
-  # If the basis_of_record argument was (not) provided:
-  if(is.null(basis_of_record)){
-    basis_of_record <- c(
-      "OBSERVATION",
-      "HUMAN_OBSERVATION",
-      "MATERIAL_SAMPLE",
-      "LITERATURE",
-      "PRESERVED_SPECIMEN",
-      "UNKNOWN",
-      "MACHINE_OBSERVATION"
+  # Test that coord_unc (when provided) is of class numeric
+  if (!is.null(coord_unc)) {
+    assertthat::assert_that(is.numeric (coord_unc),
+                            msg = "coord_unc should be of class numeric."
     )
-
-    warning(paste0(
-      "The basis_of_record parameter was not provided, the following",
-      " record types will be included: ", paste(basis_of_record, collapse = ", ")
-    ))
   }
 
-# Create a dataframe with the accepted species name and corresponding acceptedTaxonkey
-# This part is only useful when the species column can be different from the name of the accepted species
-  species_info <- gbif_data %>%
-    dplyr::filter(.data$taxonRank %in% c("SPECIES", "VARIETY"),
-                  .data$taxonomicStatus == "ACCEPTED") %>%
-    dplyr::distinct(.data$acceptedTaxonKey,
-                    .data$genus,
-                    .data$specificEpithet) %>%
-    dplyr::mutate(acceptedScientificName_2 = paste(.data$genus, .data$specificEpithet)) %>%
-    dplyr::rename(acceptedTaxonKey_2 = "acceptedTaxonKey") %>%
-    dplyr::distinct(.data$acceptedTaxonKey_2, .data$acceptedScientificName_2)
+  # Test that identification_verification_status (when provided) is of class character
+  if (!is.null(identification_verification_status)) {
+    assertthat::assert_that(is.character(identification_verification_status),
+                            msg = "identification_verification_status should be of class character."
+    )
+  }
 
 
-  data_cleaned <- gbif_data %>%
-    dplyr::left_join(species_info, by = c("species" = "acceptedScientificName_2")) %>%
-    dplyr::mutate(acceptedTaxonKey = .data$acceptedTaxonKey_2) %>%
+  #-----------------------------------------
+  #           2. Download data
+  #-----------------------------------------
+  expected_columns <- c("acceptedTaxonKey",
+                        "species",
+                        "decimalLatitude",
+                        "decimalLongitude",
+                        "establishmentMeans",
+                        "coordinateUncertaintyInMeters",
+                        "basisOfRecord",
+                        "taxonRank",
+                        "taxonomicStatus",
+                        "genus",
+                        "specificEpithet",
+                        "eventDate",
+                        "occurrenceStatus",
+                        "gbifID",
+                        "year",
+                        "countryCode",
+                        "identificationVerificationStatus",
+                        "issue")
+
+
+  zip_data <- readr::read_tsv(unz(zip_file, "occurrence.txt"),
+                              col_types = c(decimalLatitude = readr::col_number(),
+                                            decimalLongitude = readr::col_number(),
+                                            establishmentMeans = readr::col_character(),
+                                            acceptedTaxonKey = readr::col_integer()),
+                              col_select = expected_columns)
+
+  #Check that all the required columns are present
+  assertthat::assert_that(all(expected_columns %in% names(zip_data)),
+                          msg = paste("The following columns are missing from the occurrence data:",
+                                      paste(expected_columns[!expected_columns %in% names(zip_data)], collapse = ", ")))
+
+  #Check that the zip file contains data
+  assertthat::assert_that(nrow(zip_data)!=0,
+                          msg = paste("No data were found in the specified zip file.")
+  )
+
+
+
+  #-----------------------------------------
+  #         3  . Clean data
+  #-----------------------------------------
+  #We will remove data with default geospatial issues, that are not PRESENT, and that don't have coordinates
+  #This was done during the download of the get_data_gbif function but has to be specified separately here
+  issues_to_discard <- c(
+    "ZERO_COORDINATE",
+    "COORDINATE_OUT_OF_RANGE",
+    "COORDINATE_INVALID",
+    "COUNTRY_COORDINATE_MISMATCH"
+  )
+
+  zip_data<- zip_data %>%
     dplyr::filter(
-      !is.na(.data$acceptedTaxonKey), #Remove occurrences of species for which the species column does not correspond accepted species (ASN_2)
-      !is.na(.data$eventDate),
-      !is.na(.data$decimalLatitude),
-      .data$eventDate >= "1901-01-01",
-      .data$basisOfRecord %in% basis_of_record,
-      .data$coordinateUncertaintyInMeters <= coord_unc |
-        is.na(.data$coordinateUncertaintyInMeters),
-      .data$occurrenceStatus == "PRESENT") %>%
-    dplyr::select("gbifID",
-                  "year",
-                  "acceptedTaxonKey",
-                  "decimalLatitude",
-                  "decimalLongitude",
-                  "coordinateUncertaintyInMeters",
-                  "countryCode") %>%
-    dplyr::mutate(year_cat = dplyr::case_when(year <= 1925 ~ "1901-1925",
-                                              year <= 1950 ~ "1926-1950",
-                                              year <= 1975 ~ "1951-1975",
-                                              year <= 2000 ~ "1976-2000",
-                                              year > 2000 ~ "2001-2025",
-                                              TRUE ~ NA_character_)) %>%
-    dplyr::group_by(.data$year_cat,
-                    .data$acceptedTaxonKey,
-                    .data$decimalLatitude,
-                    .data$decimalLongitude) %>%
-    dplyr::summarize(n_obs = dplyr::n()) %>%
-    dplyr::ungroup() %>%
-    dplyr::left_join(species_info, by = c("acceptedTaxonKey" = "acceptedTaxonKey_2")) %>%
-    dplyr::rename(acceptedScientificName = "acceptedScientificName_2")
+      !stringr::str_detect(.data$issue,  paste(issues_to_discard, collapse = "|")),
+      .data$occurrenceStatus == "PRESENT",
+      !is.na(.data$decimalLongitude) & !is.na(.data$decimalLatitude)
+    )
 
   assertthat::assert_that(nrow(data_cleaned)!=0,
       msg=paste0("No useful data for ",
