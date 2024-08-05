9cb54e80f7c7fd136302ec866fe34f7adc73ea2c
soriadelva <soria.delva@gmail.com>
2024-08-05 15:04:17 +0200

    Add assert_that tests to test function arguments

--- R/data_stitching.R
+++ R/data_stitching.R
@@ -3,3 +3,26 @@ data_stitching <- function(x,
                            scientific_name
 ) {
 
+  #-----------------------------------------
+  # 1. Test function arguments
+  #-----------------------------------------
+
+  # Test that x is of class data.frame
+  assertthat::assert_that(inherits(x, "data.frame"),
+                          msg = paste( "x is not of class data.frame."
+                          )
+  )
+
+  #Test that y is of class data.frame
+  assertthat::assert_that(inherits(y, "data.frame"),
+                          msg = paste( "y is not of class data.frame."
+                          )
+  )
+
+  #Test that scientific name is of class character
+  assertthat::assert_that(is.character(scientific_name),
+                          msg = paste( "scientific_name is not of class character."
+                          )
+  )
+
+
